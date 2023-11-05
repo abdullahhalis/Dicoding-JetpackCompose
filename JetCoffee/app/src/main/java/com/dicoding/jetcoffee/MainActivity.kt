774f6e53106b4d3df7d9e5837095eeb1cac9c3d1
Abdullah Hali Saputra <abdullahhali060@gmail.com>
2023-11-06 02:38:37 +0700

    practice layouting on conpose

---
+++ JetCoffee/app/src/main/java/com/dicoding/jetcoffee/MainActivity.kt
@@ -0,0 +1,192 @@
+package com.dicoding.jetcoffee
+
+import android.annotation.SuppressLint
+import android.icu.text.CaseMap.Title
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dicoding.jetcoffee.model.BottomBarItem
+import com.dicoding.jetcoffee.model.Menu
+import com.dicoding.jetcoffee.model.dummyBestSellerMenu
+import com.dicoding.jetcoffee.model.dummyCategory
+import com.dicoding.jetcoffee.model.dummyMenu
+import com.dicoding.jetcoffee.ui.components.CategoryItem
+import com.dicoding.jetcoffee.ui.components.HomeSection
+import com.dicoding.jetcoffee.ui.components.MenuItem
+import com.dicoding.jetcoffee.ui.components.Search
+import com.dicoding.jetcoffee.ui.components.SectionText
+import com.dicoding.jetcoffee.ui.theme.JetCoffeeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetCoffeeTheme {
+                JetCoffeeApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun JetCoffeeApp() {
+    Scaffold(
+        bottomBar = { BottomBar() }
+    ) { innerPadding ->
+        Column(modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(innerPadding)
+        ) {
+            Banner()
+            HomeSection(
+                title = stringResource(id = R.string.section_category),
+                content = { CategoryRow() }
+            )
+            HomeSection(
+                title = stringResource(id = R.string.section_favorite_menu),
+                Modifier,
+                content = { MenuRow(listMenu = dummyMenu) }
+            )
+            HomeSection(
+                title = stringResource(id = R.string.section_best_seller_menu),
+            ){
+                MenuRow(listMenu = dummyBestSellerMenu)
+            }
+        }
+    }
+}
+
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        Search()
+    }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category = category)
+        }
+    }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu = menu)
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.background,
+        modifier = modifier
+    ) {
+        val navigationItems = listOf(
+            BottomBarItem(
+                title = stringResource(id = R.string.menu_home),
+                icon = Icons.Default.Home
+            ),
+            BottomBarItem(
+                title = stringResource(id = R.string.menu_favorite),
+                icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(id = R.string.menu_profile),
+                icon = Icons.Default.AccountCircle
+            )
+        )
+        navigationItems.map {
+            NavigationBarItem(
+                selected = it.title == navigationItems[0].title,
+                onClick = {  },
+                icon = { Icon(
+                    imageVector = it.icon,
+                    contentDescription = it.title
+                ) }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BannerPreview() {
+    Banner()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryRowPreview() {
+    CategoryRow()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MenuRowPreview() {
+    MenuRow(listMenu = dummyMenu)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JetCoffeeAppPreview() {
+    JetCoffeeTheme {
+        JetCoffeeApp()
+    }
+}
